d11e640e6689b1ac28190511d45a4fbe711a9ea5
lin93h <h_lin93@aliyun.com>
2017-11-09 23:27:03 +0800

    scroll

--- resource/js/index.js
+++ resource/js/index.js
@@ -6,4 +6,20 @@ $(function () {
     $('.page_content').on('click', function () {
         $('.mobile_nav_list').slideUp()
     })
+
+    //平滑滚动锚点
+    $('a[href*=#],area[href*=#]').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+          var $target = $(this.hash);
+          $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+          if ($target.length) {
+            var targetOffset = $target.offset().top;
+            $('html,body').animate({
+              scrollTop: targetOffset
+            },
+            1000);
+            return false;
+          }
+        }
+      });
 })
