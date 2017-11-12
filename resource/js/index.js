817c2c0e5198c478561257286a2767c3eb0d55f8
lin93h <h_lin93@aliyun.com>
2017-11-12 23:29:13 +0800

    maodian

--- resource/js/index.js
+++ resource/js/index.js
@@ -9,21 +9,7 @@ $(function () {
 
     
 
-    //平滑滚动锚点
-    $('a[href*=#],area[href*=#]').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-          var $target = $(this.hash);
-          $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
-          if ($target.length) {
-            var targetOffset = $target.offset().top;
-            $('html,body').animate({
-              scrollTop: targetOffset
-            },
-            1000);
-            return false;
-          }
-        }
-    });
+    
 
     $.getJSON("./data/index.json", function(data) {
       console.log('本地json数据：',data)
--- resource/js/service.js
+++ resource/js/service.js
@@ -1,28 +1,30 @@
 $(function() {
     /*获取本地json数据 */
     var service_data
-    $.getJSON("../data/index.json", function(data) {
+    $.getJSON("../data/services.json", function(data) {
         service_data = data
-        //console.log('本地json数据：', service_data)
+        console.log('本地json数据：', service_data)
     })
     /*服务大类*/
     $('.cate_nav_item').mouseover(function(e) {
         $(this).addClass('active').siblings('.cate_nav_item').removeClass('active')
         var id = $(this).data('id')
         $('#item_box_'+id).addClass('item_box_active').siblings('.item_box').removeClass('item_box_active')
+        $('#service_title').text(service_data[id]['name'])
+        $('#service_desc_text').html(service_data[id]['content'])
     })
 
     /*具体业务*/
     $('.cate_list .cate_item').mouseover(function(e) {
         $(this).addClass('active').siblings('.cate_item').removeClass('active')
     })
-    $('.cate_list .cate_item').click(function(e) {
+    /*$('.cate_list .cate_item').click(function(e) {
         var num = $(this).data('number')
         $('#service_title').text(service_data[num]['name'])
         $('#service_desc_text').html(service_data[num]['content'])
-        //console.log(num)
-        //console.log(service_data[num])
-    })
+        console.log(num)
+        console.log(service_data[num])
+    })*/
 
     /*移动端导航 */
     $('.mobile_nav').on('click', function () {
